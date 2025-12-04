fcf607edf2310cf80d3392f5309edb2e47e52b06
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-04 22:16:59 +0700

    lab1_4.c
    แก้

--- lab1_2.c
+++ lab1_2.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
 
-void GetSet(int arr[], int *num);
+void GetSet(int data[], int *num);
 
-int main() 
+int main()
 {
-    
     int data[20];
     int num;
-    
+
     GetSet(data, &num);
 
-    printf("\nNumber of elements: %d\n", num);
-    printf("Elements: ");
-    for (int i = 0; i < num; i++) {
-        printf("%d ", data[i]);
+    printf("value (%d ): ", num);
+    if (num > 0)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            printf("%d ", data[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
-    
+    else
+    {
+        printf("\n invalid\n");
+    }
+
     return 0;
 }
 
-void GetSet(int arr[], int *num) {
+void GetSet(int data[], int *num)
+{
     int N;
-    
-    printf("Enter number of elements (max 20): ");
+
+    printf("input value (max20): ");
+
     if (scanf("%d", &N) != 1 || N <= 0 || N > 20) {
-        *num = 0;
-        printf("Invalid or out of range N. Operation stopped.\n");
-        
+        printf("! invalid value \n");
+        *num = 0; 
         return;
     }
-    
-    printf("Enter %d integers:\n", N);
-    for (int i = 0; i < N; i++) {
-        printf("Element %d: ", i + 1);
 
-        if (scanf("%d", &arr[i]) != 1) {
-             printf("Invalid input detected. Stopping input.\n");
-             N = i;
-            
-             break;
+    printf("%d :\n", N);
+
+    for (int i = 0; i < N; i++)
+    {
+        if (scanf("%d", &data[i]) != 1)
+        {
+            printf("! invalid %d \n", i);
+            N = i; 
+            break;
         }
     }
-    
+
     *num = N;
 }
