7ae0be59573dd934b463498306db7384b2215dd6
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-28 18:19:37 +0700

    Update basic2.3.c

--- basic2.3.c
+++ basic2.3.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int t_code ;
-    float invest_amt, apr = 0.0, interest ;
+    int tCode ;
+    float amount, apr = 0.0, interest_earned ;
 
-    if ( scanf( "%d %f", &t_code, &invest_amt ) != 2 ) {
-        return 1 ;
+    if ( scanf( "%d %f", &tCode, &amount ) == 2 ) {
+        
+        switch ( tCode ) {
+            case 1 :
+                if ( amount < 5000 ) apr = 0.03 ;
+                else apr = 0.04 ;
+                break ;
+            case 2 :
+                if ( amount < 10000 ) apr = 0.05 ;
+                else apr = 0.065 ;
+                break ;
+            case 3 :
+                apr = 0.08 ;
+                break ;
+            default :
+                printf( "Invalid Term Code\n" ) ;
+                return 0 ; 
+        }
+
+        interest_earned = amount * apr ;
+        printf( "%.2f\n", interest_earned ) ;
+
     }
-
-    switch ( t_code ) {
-        case 1 :
-            if ( invest_amt < 5000 ) apr = 0.03 ;
-            else apr = 0.04 ;
-            break ;
-
-        case 2 :
-            apr = ( invest_amt < 10000 ) ? 0.05 : 0.065 ; // ใช้ Ternary เพื่อลด Similarity
-            break ;
-
-        case 3 :
-            apr = 0.08 ;
-            break ;
-
-        default :
-            printf( "Invalid Term Code\n" ) ;
-            return 0 ;
-    }
-
-    interest = invest_amt * apr ;
-    printf( "%.2f\n", interest ) ;
 
     return 0 ;
 }
