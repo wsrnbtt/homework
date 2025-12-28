4c79c5299030ff1e89f91bb331159d85dbf68e32
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-28 15:40:25 +0700

    Update lab3.4.c

--- lab3.4.c
+++ lab3.4.c
@@ -1,49 +1,43 @@
 #include <stdio.h>
 
 struct student {
-    char name[20];
+    char name[ 20 ];
     int age;
     float gpa;
 };
 
-void GetStudent(struct student child[][10], int *room);
+void GetStudent( struct student child[][ 10 ], int *room );
 
-int main() 
-{
-    struct student children[20][10];
-    int group = 0;
+int main() {
+    struct student children[ 20 ][ 10 ];
+    int group;
 
-    GetStudent(children, &group);
+    GetStudent( children, &group );
 
     return 0;
 }
 
-void GetStudent(struct student child[][10], int *room) 
-{
-    //r (room) และ s (student)
+void GetStudent( struct student child[][ 10 ], int *room ) {
     int r, s;
 
-    printf("How many rooms?: ");
-    if (scanf("%d", room) != 1) return;
+    printf( "Input number of rooms: " );
+    if ( scanf( "%d", room ) != 1 ) return;
 
-    for (r = 0; r < *room; r++) 
-    {
-        printf("\n--- Data for Room %d ---\n", r + 1);
+    for ( r = 0; r < *room; r++ ) {
+        printf( "\n--- Room %d ---\n", r + 1 );
         
-        for (s = 0; s < 10; s++) 
-        {
-            printf("Student #%d info\n", s + 1);
+        for ( s = 0; s < 10; s++ ) {
+            printf( "Student #%d\n", s + 1 );
             
-            printf("  Name: ");
-            scanf("%s", child[r][s].name);
+            printf( " > Name: " );
+            scanf( "%s", child[ r ][ s ].name );
             
-            printf("  Age: ");
-            scanf("%d", &child[r][s].age);
+            printf( " > Age : " );
+            scanf( "%d", &child[ r ][ s ].age );
             
-            printf("  GPA: ");
-            scanf("%f", &child[r][s].gpa);
+            printf( " > GPA : " );
+            scanf( "%f", &child[ r ][ s ].gpa );
             
-            printf("--------------------\n");
         }
     }
 }
