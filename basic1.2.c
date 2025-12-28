34a25a7230e6bb9b24f3ce3a799627dc793a6130
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-28 16:36:22 +0700

    Update basic1.2.c

--- basic1.2.c
+++ basic1.2.c
@@ -7,20 +7,20 @@ int main() {
         return 1 ;
     }
 
-    if ( score >= 80 ) {
-        printf( "Grade A" ) ;
+    if ( score < 50 ) {
+        printf( "Grade F" ) ;
     } 
-    else if ( score >= 70 ) {
-        printf( "Grade B" ) ;
-    } 
-    else if ( score >= 60 ) {
-        printf( "Grade C" ) ;
-    } 
-    else if ( score >= 50 ) {
+    else if ( score < 60 ) {
         printf( "Grade D" ) ;
     } 
+    else if ( score < 70 ) {
+        printf( "Grade C" ) ;
+    } 
+    else if ( score < 80 ) {
+        printf( "Grade B" ) ;
+    } 
     else {
-        printf( "Grade F" ) ;
+        printf( "Grade A" ) ;
     }
 
     return 0 ;
