5f447a3e79871256c3d6111ebb8c3c56f3aaf08e
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-28 15:28:57 +0700

    Update lab3.2.c

--- lab3.2.c
+++ lab3.2.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 
 struct student {
-    char name[20];
-    int age;
-    char sex;
-    float gpa;
-};
+    char name[ 20 ] ;
+    int age ;
+    char sex ;
+    float gpa ;
+} ;
 
-void upgrade(struct student *child);
+void upgrade( struct student *child ) ;
 
-int main() 
-{
-    struct student aboy;
+int main() {
+    struct student aboy ;
+    aboy.sex = 'M' ;
+    aboy.gpa = 3.00 ;
 
-    // กำหนดค่าเริ่มต้นให้กับตัวแปร aboy
-    aboy.sex = 'M';
-    aboy.gpa = 3.00;
+    upgrade( &aboy ) ;
 
-    upgrade(&aboy);
-
-    // แสดงผลลัพธ์ทศนิยม 2 ตำแหน่ง
-    printf("%.2f", aboy.gpa);
-
-    return 0;
+    printf( "%.2f", aboy.gpa ) ;
+    
+    return 0 ;
 }
 
-void upgrade(struct student *child) 
-{
-    if (child->sex == 'M') 
-    {
-        child->gpa = child->gpa + (child->gpa * 0.10);
+void upgrade( struct student *child ) {
+    if ( child->sex == 'M' ) {
+        child->gpa = child->gpa * 1.10f ;
     } 
-    else if (child->sex == 'F') 
-    {
-        // เพิ่ม 20% สำหรับนักเรียนหญิง
-        child->gpa = child->gpa + (child->gpa * 0.20);
+    else if ( child->sex == 'F' ) {
+        child->gpa = child->gpa * 1.20f ;
     }
 }
