20eefacb4263942f399d66dd17532ef155662ac5
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-29 21:37:35 +0700

    Create basic4_1.c

---
+++ basic4_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void printReverse(int arr[], int size) {
+    int j;
+    for (j = size - 1; j >= 0; j--) {
+        printf("%d\n", arr[j]);
+    }
+}
+
+int main() {
+    int total_elements, i;
+
+    if (scanf("%d", &total_elements) != 1 || total_elements <= 0) {
+        return 0; 
+    }
+
+    int data_list[total_elements];
+
+    for (i = 0; i < total_elements; i++) {
+        if (scanf("%d", &data_list[i]) != 1) {
+            break;
+        }
+    }
+    printf("----------------------\n");
+    printReverse(data_list, total_elements);
+
+    return 0;
+}
