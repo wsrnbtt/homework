8e58f1d9175e9b50e380f6bade30c34be120fa63
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-29 18:19:35 +0700

    Create basic3_2.c

---
+++ basic3_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int id;
+    float gpa;
+};
+
+int main() {
+    int total_students, idx;
+
+    if (scanf("%d", &total_students) != 1) {
+        return 0;
+    }
+
+    struct Student list[total_students];
+
+    for (idx = 0; idx < total_students; idx++) {
+        // รับค่า: ID, Score, Name
+        if (scanf("%d %f %s", &list[idx].id, &list[idx].gpa, list[idx].name) != 3) {
+            break;
+        }
+    }
+
+    for (idx = 0; idx < total_students; idx++) {
+        printf("ID: %d, Score: %.2f, Name: %s\n", list[idx].id, list[idx].gpa, list[idx].name);
+    }
+
+    return 0;
+}
