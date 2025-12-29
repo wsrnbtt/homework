3becc1aaecde8375ed50ac2a49e5527ca1615f8c
wsrnbtt <wisarut_no67@live.rmutl.ac.th>
2025-12-29 22:02:41 +0700

    Create basic4_3.c

---
+++ basic4_3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define DAYS 7
+
+int main() {
+    int daily_temp[DAYS];
+    int max_temp;
+    int i;
+
+    for (i = 0; i < DAYS; i++) {
+        if (scanf("%d", &daily_temp[i]) != 1) {
+            return 1;
+        }
+    }
+
+    max_temp = daily_temp[0];
+    for (i = 1; i < DAYS; i++) {
+        if (daily_temp[i] > max_temp) {
+            max_temp = daily_temp[i];
+        }
+    }
+
+    printf("\n DAILY TEMPERATURE REPORT \n");
+    printf("Recorded Temperatures (C): ");
+    for (i = 0; i < DAYS; i++) {
+        printf("%d", daily_temp[i]);
+        if (i < DAYS - 1) printf(" ");
+    }
+    printf("\n");
+
+    printf("Maximum Temperature Found: %d C\n", max_temp);
+
+    if (max_temp >= 35) {
+        printf("Weather is HOT!\n");
+    } else {
+        printf("Weather is MODERATE.\n");
+    }
+
+    return 0;
+}
