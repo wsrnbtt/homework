acb56c4b038fe4e79675e5d5120d9f1e26f067e6
Wisarut Noibut <wisarut_no67@live.rmutl.ac.th>
2026-03-17 19:45:03 +0700

    calcula

--- basic1.2.c
+++ basic1.2.c
@@ -1,27 +1,2 @@
 #include <stdio.h>
-
-int main() {
-    int score ;
-
-    if ( scanf( "%d", &score ) != 1 ) {
-        return 1 ;
-    }
-
-    if ( score < 50 ) {
-        printf( "Grade F" ) ;
-    } 
-    else if ( score < 60 ) {
-        printf( "Grade D" ) ;
-    } 
-    else if ( score < 70 ) {
-        printf( "Grade C" ) ;
-    } 
-    else if ( score < 80 ) {
-        printf( "Grade B" ) ;
-    } 
-    else {
-        printf( "Grade A" ) ;
-    }
-
-    return 0 ;
-}
+fdf
